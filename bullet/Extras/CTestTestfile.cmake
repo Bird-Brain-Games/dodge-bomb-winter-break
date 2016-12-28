80507ae87216207bb3b9c866884b8e06921a0a6a
Goombario <Goombario@localhost>
2016-12-28 16:16:19 -0500

    Added bullet libraries

---
+++ bullet/Extras/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: C:/Hg/bullet-test/bullet3-2.85.1/Extras
+# Build directory: C:/Hg/winter-break/bullet/Extras
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Serialize")
+subdirs("ConvexDecomposition")
+subdirs("HACD")
+subdirs("GIMPACTUtils")
+subdirs("InverseDynamics")
---
+++ bullet/examples/ExampleBrowser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Hg/bullet-test/bullet3-2.85.1/examples/ExampleBrowser
+# Build directory: C:/Hg/winter-break/bullet/examples/ExampleBrowser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
