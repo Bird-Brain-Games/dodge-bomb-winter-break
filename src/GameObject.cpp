e005701fad9f1ccb2cbff859fc714bcdd0c39177
Goombario <Goombario@localhost>
2016-12-28 19:32:59 -0500

    Switched to freeGLUT Initialized basic test objects

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,56 +1,28 @@
 #include "GameObject.h"
-#include <GLM\gtx\rotate_vector.hpp>
 
-GameObject::GameObject(LoadObject * _obj)
+GameObject::GameObject(LoadObject * _model, btRigidBody *_body)
 {
-	obj = _obj; // reuse that there memory
-	position = glm::vec3(0.0, 0.0, 0.0);
-	velocity = glm::vec3(0.0, 0.0, 0.0);
-	yRotation = 0.0f;
+	model = _model;
+	body = _body;
 }
 
-GameObject::GameObject(char * filepath)
+GameObject::~GameObject()
 {
-	obj = new LoadObject();
-	obj->loadFromObject(filepath);
-	position = glm::vec3(0.0, 0.0, 0.0);
-	velocity = glm::vec3(0.0, 0.0, 0.0);
-	yRotation = 0.0f;
+	delete model;
+	delete body->getMotionState();
+	delete body;
+
+	model = nullptr;
+	body = nullptr;
 }
 
-void GameObject::draw(Shader *s)
+void GameObject::draw(Shader* s)
 {
-	// bind tex here if you had one
-
-	// compute local transformation
-	glm::mat4x4 rotation = glm::rotate(yRotation, glm::vec3(0.0, 1.0, 0.0));
-	glm::mat4x4 translation = glm::translate(position);
-
-	glm::mat4x4 transform = translation * rotation;
-	s->uniformMat4x4("localTransform", &transform);
-
-	obj->draw();
+	btTransform worldTransform = body->getWorldTransform();
+	//worldTransform.
 }
 
-void GameObject::update(float deltaT)
+void GameObject::update(float dt)
 {
-	position = position + (velocity*deltaT);
+
 }
-
-
-void GameObject::setPos(glm::vec3 const & _set) { position = _set; }
-void GameObject::setVel(glm::vec3 const & _set) { velocity = _set; }
-void GameObject::setAcc(glm::vec3 const & _set) { acc = _set; }
-void GameObject::setRot(glm::vec3 const & _set) { rot = _set; }
-void GameObject::setMass(float _mass) { mass = _mass; }
-
-void GameObject::addPos(glm::vec3 const & _set) { position += _set; }
-void GameObject::addVel(glm::vec3 const & _set) { velocity += _set; }
-void GameObject::addAcc(glm::vec3 const & _set) { acc += _set; }
-void GameObject::addRot(glm::vec3 const & _set) { rot += _set; }
-
-glm::vec3 const & GameObject::getPos() const { return  position; }
-glm::vec3 const & GameObject::getVel() const { return  velocity; }
-glm::vec3 const & GameObject::getAcc() const { return  acc; }
-glm::vec3 const & GameObject::getRot() const { return  rot; }
-glm::vec3 const & GameObject::getScale() const { return scale; }
