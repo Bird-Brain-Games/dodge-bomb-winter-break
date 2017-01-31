70804b32b195e32d1883596170b8958ac9cd185e
Goombario <Goombario@localhost>
2017-01-31 10:20:24 -0500

    cleaned up comments TODO: detection, shapes, active rigid bodies

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -43,13 +43,6 @@ void GameObject::draw(Shader* s)
 
 	// Compute local transformation
 	s->uniformMat4x4("localTransform", &body->getWorldTransform());
-
-	/*btTransform t;
-	btScalar m[16];
-	body->getMotionState()->getWorldTransform(t);
-	t.getOpenGLMatrix(m);
-
-	s->uniformMat4x4("localTransform", &glm::make_mat4x4((float*)m));*/
 	
 	model->draw();
 }
